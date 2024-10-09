a03bfc4f2c634ad3ba88ef8e3e89f999eb9e3cd7
Martin Janevski <mjanevski020@gmail.com>
2024-10-09 12:03:08 +0200

    Dodavanje na skok i idle animacija so funkcionalnost vo kod. Privremeno brisenje na word dokumentacijata dodeka ne se zavrsi (sozdava temp fajlovi sekoja promena)

--- TimskiProektGodotFile/Scripts/Player.gd
+++ TimskiProektGodotFile/Scripts/Player.gd
@@ -1,28 +1,52 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
+const JUMP_VELOCITY = -600.0
+const ACCELERATION = 1200
+const FRICTION = 2000
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
-	# Add the gravity.
+	apply_gravity(delta)
+	var input_axis = Input.get_axis("walk_left", "walk_right")
+	handle_jump()
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	update_animations(input_axis)
+	move_and_slide()
+
+func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
+			
+func handle_acceleration(input_axis, delta):
+	if not is_on_floor(): return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x,  SPEED * input_axis,  ACCELERATION * delta)
+		
+func apply_friction(input_axis, delta):
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0,  FRICTION * delta)
+		
+func update_animations(input_axis):
+	if not is_on_floor(): 
+		#flip direction if action pressed
+		if(Input.is_action_pressed("walk_left") or Input.is_action_pressed("walk_right")):
+			animated_sprite_2d.flip_h = (input_axis < 0)	
+		if velocity.y != 0.0:
+			animated_sprite_2d.play("jump")
+	elif is_on_floor() and input_axis !=0:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("walk")	
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		animated_sprite_2d.play("idle")
+	
