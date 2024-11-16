bdae25dd9b3bc6c4fa18087fe110173c2665d2a0
Martin Janevski <mjanevski020@gmail.com>
2024-11-16 17:56:56 +0100

    Dodavanje na CoyoteJump timer I logika.

--- TimskiProektGodotFile/Scripts/Player.gd
+++ TimskiProektGodotFile/Scripts/Player.gd
@@ -6,30 +6,35 @@ const JUMP_VELOCITY = -600.0
 const ACCELERATION = 1200.0
 const AIR_ACCELERATION = 600.0
 const FRICTION = 2000.0
-var air_jump = false
 const AIR_RESISTANCE = 1500.0
+var air_jump = false
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $"CoyoteJump Timer"
 
 func _physics_process(delta):
 	apply_gravity(delta)
 	var input_axis = Input.get_axis("walk_left", "walk_right")
 	handle_jump()
-	handle_acceleration(input_axis, delta)	
+	handle_acceleration(input_axis, delta)
 	handle_air_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
-	update_animations(input_axis)
 	apply_air_resistance(input_axis, delta)
+	update_animations(input_axis)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
 
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
 func handle_jump():
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		air_jump = true
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = JUMP_VELOCITY
@@ -39,7 +44,7 @@ func handle_jump():
 			
 		if Input.is_action_just_pressed("jump") and air_jump:
 			velocity.y = JUMP_VELOCITY * 0.8
-			air_jump = false	
+			air_jump = false
 			
 func handle_acceleration(input_axis, delta):
 	if not is_on_floor(): return
@@ -54,11 +59,11 @@ func handle_air_acceleration(input_axis, delta):
 func apply_friction(input_axis, delta):
 	if input_axis == 0 and is_on_floor():
 		velocity.x = move_toward(velocity.x, 0,  FRICTION * delta)
+		
 func apply_air_resistance(input_axis, delta):
 	if input_axis == 0 and not is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, AIR_RESISTANCE * delta)
 
-		
 func update_animations(input_axis):
 	if not is_on_floor(): 
 		#flip direction if action pressed
