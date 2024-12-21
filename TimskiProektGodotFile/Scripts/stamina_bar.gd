c4a2d86d7abb5a9654c0e3735a4994b119d97287
Martin Janevski <mjanevski020@gmail.com>
2024-12-21 12:56:09 +0100

    Inicjalen setup na Stamina Bar.

---
+++ TimskiProektGodotFile/Scripts/stamina_bar.gd
@@ -0,0 +1,12 @@
+extends TextureProgressBar
+
+
+
+@export var jump_drain = 20.0
+@export var dash_drain = 40.0
+@export var wall_jump_drain = 15.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
