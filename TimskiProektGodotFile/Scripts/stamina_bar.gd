62014a0316fffa418421830a56f030c50c8ac3ed
Martin Janevski <mjanevski020@gmail.com>
2024-12-22 10:28:42 +0100

    Implementiranje StaminaBar funkcionalnost pri skok, dvoen skok, skok od dzid I dash.

--- TimskiProektGodotFile/Scripts/stamina_bar.gd
+++ TimskiProektGodotFile/Scripts/stamina_bar.gd
@@ -1,12 +1,28 @@
 extends TextureProgressBar
 
-
-
+@onready var show_hide_timer = $"ShowHideTimer"
 @export var jump_drain = 20.0
 @export var dash_drain = 40.0
 @export var wall_jump_drain = 15.0
 
-
+func _ready():
+	value = 100
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(Input.is_action_just_pressed('jump') or Input.is_action_just_pressed('dash_left') or Input.is_action_just_pressed('dash_right')):
+		turn_visible()
+		show_hide_timer.start()	
+	if(show_hide_timer.is_stopped()):
+		value += 1
+	if value == 100:
+		turn_invisible()
+			
+func turn_invisible():
+	create_tween().tween_property(self, 'modulate:a', 0, 0.3)
+	
+func turn_visible():
+	create_tween().tween_property(self, 'modulate:a', 1, 0.3)
+
+
+func _on_show_hide_timer_timeout():
+	show_hide_timer.stop()
